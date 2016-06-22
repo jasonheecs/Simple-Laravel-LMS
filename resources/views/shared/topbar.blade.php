4a7bb83e9755789424b5f8a6b911099d95c727a4
Jason Hee <jasonheecs@gmail.com>
2016-06-23 00:16:52 +0800

    Responsive tweaks

--- resources/views/shared/topbar.blade.php
+++ resources/views/shared/topbar.blade.php
@@ -1,8 +1,8 @@
 <div class="topbar__wrapper flex flex--middle flex--space-between">
     <div class="topbar">
-        <div class="topbar__content flex flex--middle flex--wrap">
-            <div class="text--center">
-                <a href="{{ url('/home') }}">
+        <div class="topbar__content flex flex--middle">
+            <div class="topbar__logo-wrapper">
+                <a href="{{ url('/home') }}" class="topbar__logo-link">
                     <img class="topbar__logo" src="{{ asset('img/efusion-logo.png') }}" alt="Efusion Technology" />
                 </a>
                 <h3 class="topbar__subtitle">Learning Management System</h3>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,57 +11,59 @@
 @stop
 
 @section('content')
-<div id="users-panel" class="panel panel--default">
-    <div class="users__header flex flex--space-between flex--middle">
-        <div>@include('svg.user'){{ count($users) }} Users</div>
-        @if (Auth::user()->canManageUsers())
-            <ul id="user-admin-actions" class="list list--inline button-group button-group--right">
-                <li>
-                    <form method="GET" action="{{ url('/users/create') }}">
-                        <button id="create-user-btn" class="btn btn--primary" type="submit"><i class="icon icon--create-user"></i> Create User</button>
-                    </form>
-                </li>
-            </ul>
-        @endif
-    </div>
+<div class="container">
+    <div id="users-panel" class="panel panel--default">
+        <div class="users__header flex flex--space-between flex--middle">
+            <div>@include('svg.user'){{ count($users) }} Users</div>
+            @if (Auth::user()->canManageUsers())
+                <ul id="user-admin-actions" class="list list--inline button-group button-group--right">
+                    <li>
+                        <form method="GET" action="{{ url('/users/create') }}">
+                            <button id="create-user-btn" class="btn btn--primary" type="submit"><i class="icon icon--create-user"></i> Create User</button>
+                        </form>
+                    </li>
+                </ul>
+            @endif
+        </div>
 
-    <div class="padding--bottom-lg">
-        <table id="users-list" class="table table--bordered table--hover table--responsive">
-            <thead>
-                <tr>
-                    <th class="text--center"><input type="checkbox" name="checkall" /></th>
-                    <th>&nbsp;</th>
-                    <th>&nbsp;</th>
-                    <th>Name</th>
-                    <th>Company</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach ($users as $user)
-                <tr>
-                    <td class="text--center"><input type="checkbox" /></td>
-                    <td class="users__status-icon">
-                        @if ($user->is('superadmin'))
-                            @include('svg.star')
-                        @elseif ($user->is('admin'))
-                            @include('svg.hammer')
-                        @endif
-                    </td>
-                    <td class="users__avatar-container">
-                        @if ($user->avatar)
-                        <a href="/users/{{ $user->id }}">
-                            <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="users__avatar" width="28" height="28">
-                        </a>
-                        @endif
-                    </td>
-                    <td data-th="Name"><a href="/users/{{ $user->id }}">{{ $user->name }}</a></td>
-                    <td data-th="Company">{{ $user->company }}</td>
-                    <td data-th="Email"><a href="mailto:{{ $user->email }}" class="link--muted">{{ $user->email }}</a></td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+        <div class="padding--bottom-lg">
+            <table id="users-list" class="table table--bordered table--hover table--responsive">
+                <thead>
+                    <tr>
+                        <th class="text--center"><input type="checkbox" name="checkall" /></th>
+                        <th>&nbsp;</th>
+                        <th>&nbsp;</th>
+                        <th>Name</th>
+                        <th>Company</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td class="users__checkbox"><input type="checkbox" /></td>
+                        <td class="users__status-icon">
+                            @if ($user->is('superadmin'))
+                                @include('svg.star')
+                            @elseif ($user->is('admin'))
+                                @include('svg.hammer')
+                            @endif
+                        </td>
+                        <td class="users__avatar-container">
+                            @if ($user->avatar)
+                            <a href="/users/{{ $user->id }}">
+                                <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="users__avatar" width="28" height="28">
+                            </a>
+                            @endif
+                        </td>
+                        <td data-th="Name"><a href="/users/{{ $user->id }}">{{ $user->name }}</a></td>
+                        <td data-th="Company">{{ $user->company }}</td>
+                        <td data-th="Email"><a href="mailto:{{ $user->email }}" class="link--muted">{{ $user->email }}</a></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @stop
